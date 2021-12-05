4c0f2d9aa4514795d8e37fe0873bc0ff040e9983
Sergiu78 <turutaserghei@mail.ru>
2021-12-05 17:57:45 +0200

    adding bootstrap and main page

--- main.go
+++ main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func home_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "hello first page go")
+type User struct {
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
+	Hobbies               []string
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is : %s. He is %d , "+
+		"and he has money : %d", u.Name, u.Age, u.Money)
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Alex", 25, 500, 4.2, 5.8, []string{"Football", "Karate", "Programming"}}
+	// fmt.Fprintf(w, bob.getAllInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts page")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8000", nil)
 }
 
 func main() {
-	http.HandleFunc("/", home_page)
-	http.ListenAndServe(":8000", nil)
+	handleRequest()
 }
